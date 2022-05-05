2406766db5ff1954a1d0fa69ff855aa612db84d1
Richard <b00121651@mytudublin.ie>
2022-05-05 14:14:48 +0100

    UNIT TEST

---
+++ unittestexample.py
@@ -0,0 +1,43 @@
+import unittest
+
+import urllib.request
+
+
+class BasicTests(unittest.TestCase):
+
+
+
+    def test_main_page(self):
+        
+        status_code = urllib.request.urlopen("http://localhost:5000").getcode()
+
+   
+        self.assertEqual(status_code, 200)
+   
+
+    def test_page2(self):
+        
+        status_code = urllib.request.urlopen("http://localhost:5000/page2").getcode()
+
+   
+        self.assertEqual(status_code, 200)
+		
+		
+    def test_main_page3(self):
+        
+        status_code = urllib.request.urlopen("http://localhost:5000/page3").getcode()
+
+   
+        self.assertEqual(status_code, 200)
+   
+
+    def test_getnumbers(self):
+        
+        status_code = urllib.request.urlopen("http://localhost:5000/getNumber").getcode()
+
+   
+        self.assertEqual(status_code, 200)
+   
+
+if __name__ == "__main__":
+    unittest.main()
